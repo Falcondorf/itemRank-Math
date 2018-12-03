1008cf39b70785f7d870116bcde7a5a01342fc26
Falcondorf <pikirby45@gmail.com>
2018-12-03 09:04:22 +0100

    Starting project - Initialize prototype

---
+++ itemrank_module.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#A = np.matrix / alpha = float / v = np.array / m = bool / return np.matrix
+def itemRank(A, alpha, v, m):
+    #TODO verifier valeur en param sont corrects
+    print("Verification des variables paramètre")
+
+    x = np.ndarray
+    if m:
+        print("Resolution par recurrence")
+        #TODO Score par recurrence
+
+    else :
+        print("resolution par inversion matricielle")
+        #TODO Score par inversion matricielle
+
+    return x #vecteur des scores d'importance des noeuds
