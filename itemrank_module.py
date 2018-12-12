0ed18479ccd0ad04659827c9eb42ec0568be8731
Falcondorf <pikirby45@gmail.com>
2018-12-12 14:25:39 +0100

    correcting calculate_D + fix variables verification + dev formulas

--- itemrank_module.py
+++ itemrank_module.py
@@ -1,17 +1,17 @@
 import numpy as np # Shift+F1 pour ouvrir la doc
 import csv
-from copy import copy
+import copy
 
 #Calcule la matrice diagonale contenant les degres des noeuds de la matrice A
 def calculate_D(A):
     D = np.zeros(A.shape) #Cree une matrice de meme forme remplie de 0
-    #TODO remplir la diagonale avec le degres des noeuds
     for i in range(0,A.shape[0]):
         for j in range(0, A.shape[1]):
             if (A[i,j] != 0):
                 D[i,i] = D[i,i] + 1
 
-    return D
+    D1 = np.matrix(D)
+    return D1
 
 #Lecture d'un ficheir CSV (fn en paramètre) du projet et le transforme en np.array
 def read_csv_toarray(fn):
@@ -28,26 +28,31 @@ def read_csv_toarray(fn):
 #A = np.matrix / alpha = float / v = np.array / m = bool / return np.matrix
 def itemRank(A, alpha, v, m):
     print("Verification des variables paramètre")
-    if (alpha > 0 or alpha < 1):
-        raise ValueError("alpha doit être entre 0 et 1!!")
+    if (alpha < 0 or alpha > 1):
+        raise ValueError("alpha doit être entre 0 et 1!! <value>=", alpha)
 
     if (A.shape[0] != A.shape[1]):
         raise ValueError("La matrice doit être carree!!")
 
+    print("OK")
+
+    D = calculate_D(A)  # Calculer matrice diagonale contenant les degres des noeuds
+    P = D.I * A  # Matrice de probabilite de transition
     if m:
         print("Resolution par recurrence")
         #TODO Score par recurrence
-        D = calculate_D(A) #Calculer matrice diagonale contenant les degres des noeuds
-        P = D.I * A #Matrice de probabilite de transition
-        I = np.identity(P.shape[0]) #Matrice d'identite de P
-        convergent = (1-alpha) * (I - alpha * P.T).I * v
         x = copy.deepcopy(v)
-        while(0): #Tant que non convergence
-            x = alpha * P.T * x  + (1-alpha) * v
+        converge = False
+        while(not converge): #Tant que non convergence
+            tmp_x = copy.deepcopy(x)
+            x = alpha * P.T.A * x + (1 - alpha) * v
+            if (np.allclose(tmp_x, x)): #Compare les deux vecteurs afin de savoir s'ils sont à peu près les même valeurs (On acceptera qu'il y a convergence)
+               converge == True
+
     else :
         print("resolution par inversion matricielle")
-        #TODO Score par inversion matricielle
-        x = []
+        I = np.identity(P.shape[0])  # Matrice d'identite de P
+        x = (1 - alpha) * np.linalg.inv(I - alpha * P.T.A) * v
 
     return x #vecteur des scores d'importance des noeuds
 
@@ -56,15 +61,17 @@ def itemRank(A, alpha, v, m):
 a = np.array([[1,2,3],[4,5,6],[8,8,8]], np.int32) #int32 pas necessaire... Juste indicatif
 print("Print test array\n",a)
 print("Print element at [1,1]",a[1,1])
-print("Print element at [1,0]", a[1,0])
-
+print("Print element at [1,0]", a[1,0])#
+abis = np.array([1,2,3])
+print("Print 1D array:",abis)
 b = np.matrix([[1,2,4],[7,8,3],[3,3,2]])
 print("Print test Matrix\n",b)
 print("Matrix shape=", b.shape)
 print("Square matrix? -> row:", b.shape[0],"col:", b.shape[1], "\n Is same?", b.shape[0] == b.shape[1])
 c = np.matrix([[1,3,5],[0,1,0],[3,10,1]])
+print("Multiply Matrix with array: ", abis*b.A)
 print("Another matrix c:\n", c)
-print("matrix a x matrix b:\n",a*b)
+print("matrix a x matrix b:\n", a*b)
 c[1,1] = 0
 print("C after changing [1,1] to 0\n", c)
 
@@ -77,3 +84,16 @@ print("Print multiplicative inverse of invertible matrix\n", b.I)
 print("Matrix identity size 5\n", np.identity(5))
 
 print("Read CSV file 12\n",read_csv_toarray("./Personnalisation_Student12.csv"))
+
+A = np.matrix([[0,0,1,0,0,1,1,0,1,1],
+              [0,0,1,0,1,0,1,0,1,1],
+              [1,1,0,0,1,1,1,1,1,0],
+              [0,0,0,0,1,0,0,0,1,0],
+              [0,1,1,1,0,0,0,1,0,0],
+              [1,0,1,0,0,0,1,1,1,1],
+              [1,1,1,0,0,1,0,1,1,0],
+              [0,0,1,0,1,1,1,0,0,0],
+              [1,1,1,1,0,1,1,0,0,0],
+              [1,1,0,0,0,1,0,0,0,0]])
+v = read_csv_toarray("./Personnalisation_Student12.csv")
+print("Score itemRank sur CSV 12:\n", itemRank(A, 0.85, v, False))
